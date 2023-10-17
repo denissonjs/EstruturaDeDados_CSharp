4a214cdf281a6f9b87ce718a86545f17cc6081e6
Denisson Silva <83768946+denissonjs@users.noreply.github.com>
2023-10-17 17:53:55 -0300

    Inserindo exemplo prático sobre Conjuntos

--- Estruturas_Chaveadas.cs
+++ Estruturas_Chaveadas.cs
@@ -1,3 +1,6 @@
+//
+using System;
+using System.Collection.Generic;
 internal class Estruturas_Chaveadas
 {
     class Dictionary
@@ -18,11 +21,11 @@ internal class Estruturas_Chaveadas
         dgv.Columns.Add("dgvColumn_NOME", "Nome");
         dgv.Columns.Add("dgvColumn_EMAIL", "Email");
         
-        // Criamos um array de strings com dados aleatórios e adicionamos ao DataGridView. 
+        // Criamos um array de strings com dados aleatórios e adicionamos à linha do DataGridView. 
         string[] row = new string[] { "123", "John Doe", "30", "johndoe@teste.com.br", "Estagiário" };
         dgv.Rows.Add(row);
 
-        // Agora crio meu dicionário e declarando as chaves com os mesmos nomes das colunas do DataGridView.
+        // Agora crio meu dicionário declarando as chaves com os mesmos nomes das colunas do DataGridView.
         public static Dictionary<string, string> usuario = new Dictionary<string, string>()
         {
             { "dgvColumn_ID", "ID" },
@@ -63,8 +66,35 @@ internal class Estruturas_Chaveadas
                 values = values.TrimEnd(',', ' ');
 
                 // Por fim, adiciono a sequencia de nome de colunas que o loop guardou junto a seus valores. Garantindo que cada coluna receba o valor devido.
-                query += "INSERT INTO TABELA_QUALQUER (" + columnNames + ") VALUES (" + values + "); ";
+                query += "INSERT INTO TABELA_QUALQUER (" + columnNames + ") VALUES (" + values + ")";
+                return query;
             }
         }
     }
+
+class Conjuntos
+{
+    /* A função abaixo permite bloquear vários clientes de uma vez. 
+       Para evitar múltiplas tentativas de modificação do mesmo cliente, 
+       o que poderia gerar uma demanda desnecessária no banco de dados, 
+       usamos um conjunto (HashSet) de clientes. */
+
+    public static string BloquearClientes(int[] idsClientes)
+    {
+        // Cria um conjunto de elementos únicos com a classe "HashSet<T>", que não aceita dados repetidos.
+        HashSet<int> conjuntoClientes = new HashSet<int>(idsClientes);
+
+        // Converte o conjunto de IDs de clientes em uma string formatada.
+        string idsClientesString = string.Join(",", conjuntoClientes);
+
+        // Por fim, com as duplicatas removidas e a string formatada, unimos à cláusula "update".
+        string query = $"UPDATE TABELA_CLIENTES SET BLOQUEIO = 'S' WHERE IDCLIENTE IN ({idsClientesString})";
+
+        return query;
+    }
+
+    /* Chamar a função BloquearClientes(new int[] {123, 123, 123, 321, 321,321}) irá retornar:
+       UPDATE TABELA_CLIENTES SET BLOQUEIO = 'S' WHERE IDCLIENTE IN (123, 321). */  
+}
+
 }
